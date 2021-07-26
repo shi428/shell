e57d1dce70ad7369181549b6458ecb263e4c10ff
shi428 <shi428@purdue.edu>
2021-07-25 20:46:24 -0400

    ctrl c

--- src/shell_main.cpp
+++ src/shell_main.cpp
@@ -6,11 +6,22 @@ void printPrompt() {
     cout << "shell>";
 }
 
+void sigint_handler(int signum) {
+    signal(SIGINT, sigint_handler);
+    fflush(stdout);
+    cout << endl;
+    printPrompt();
+    fflush(stdout);
+}
+
 int main(int argc, char *argv[]) {
+    signal(SIGINT, sigint_handler);
     string line;
     struct passwd *p = getpwuid(getuid());
     while (1) {
+        if (isatty(0)) {
         printPrompt();
+        }
         if (!getline(cin, line)) break;
         vector <Token> tokens = genTokens(line);
         Tree *parseTree = newTree(tokens);
